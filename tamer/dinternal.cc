9d04b597d2599bfdab02d02c38a19d1cadc9cea1
Eddie Kohler <ekohler@gmail.com>
2014-02-20 17:32:12 -0500

    Combine dbase.cc and dinternal.cc.

--- tamer/dinternal.cc
+++ tamer/dinternal.cc
@@ -1,4 +1,5 @@
-/* Copyright (c) 2012-2014, Eddie Kohler
+/* Copyright (c) 2007-2014, Eddie Kohler
+ * Copyright (c) 2007, Regents of the University of California
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -15,6 +16,83 @@
 #include <string.h>
 
 namespace tamer {
+namespace tamerpriv {
+timeval now;
+bool now_updated;
+} // namespace tamerpriv
+
+driver* driver::main;
+driver* driver::indexed[capacity];
+int driver::next_index;
+
+driver::driver() {
+    if (next_index < capacity) {
+        index_ = next_index;
+        ++next_index;
+    } else
+        for (index_ = 0; index_ != capacity && indexed[index_]; ++index_)
+            /* do nothing */;
+    assert(index_ < capacity);
+    indexed[index_] = this;
+}
+
+driver::~driver() {
+    indexed[index_] = 0;
+    if (main == this)
+        main = 0;
+}
+
+bool initialize(int flags) {
+    if (driver::main)
+        return true;
+
+    if (!(flags & (use_tamer | use_libevent | use_libev))) {
+        const char* dname = getenv("TAMER_DRIVER");
+        if (dname && strcmp(dname, "libev") == 0)
+            flags |= use_libev;
+        else if (dname && strcmp(dname, "libevent") == 0)
+            flags |= use_libevent;
+        else
+            flags |= use_tamer;
+    }
+
+    if (!driver::main && (flags & use_libev))
+        driver::main = driver::make_libev();
+    if (!driver::main && (flags & use_libevent))
+        driver::main = driver::make_libevent();
+    if (!driver::main && !(flags & use_tamer) && (flags & no_fallback))
+        return false;
+    if (!driver::main)
+        driver::main = driver::make_tamer();
+
+    if (!(flags & keep_sigpipe))
+        signal(SIGPIPE, SIG_IGN);
+    return true;
+}
+
+void cleanup() {
+    delete driver::main;
+    driver::main = 0;
+}
+
+void driver::at_delay(double delay, event<> e)
+{
+    if (delay <= 0)
+	at_asap(e);
+    else {
+	timeval tv = now();
+	long ldelay = (long) delay;
+	tv.tv_sec += ldelay;
+	tv.tv_usec += (long) ((delay - ldelay) * 1000000 + 0.5);
+	if (tv.tv_usec >= 1000000) {
+	    tv.tv_sec++;
+	    tv.tv_usec -= 1000000;
+	}
+	at_time(tv, e);
+    }
+}
+
+
 namespace tamerpriv {
 
 driver_asapset::~driver_asapset() {
